3e49131a0171a4449199a921f0573a9537b47795
xXchelikXx <162018732+xXchelikXx@users.noreply.github.com>
2025-04-06 15:04:13 +0300

    Изменил код

--- main.py
+++ main.py
@@ -5,37 +5,18 @@ from terminaltables import AsciiTable
 from dotenv import load_dotenv
 
 
-def predict_rub_salary_hh(salary):
-    if salary:
-        if salary['currency'] == 'RUR':
-            if salary['from'] and salary['to']:
-                return int((salary['from'] + salary['to']) / 2)
-            elif salary['from']:
-                return int(salary['from'] * 1.2)
-            elif salary['to']:
-                return int(salary['to'] * 0.8)
-            else:
-                return None
-        else:
-            return None
+def get_predict_rub_salary(payment_to=None, payment_from=None):
+    if payment_from and payment_to:
+        return int((payment_from + payment_to) / 2)
+    elif payment_from:
+        return int(payment_from * 1.2)
+    elif payment_to:
+        return int(payment_to * 0.8)
+    else:
+        return None
 
 
-def predict_rub_salary_sj(vacancies_sj):
-    if vacancies_sj:
-        if vacancies_sj['currency'] == 'rub':
-            if vacancies_sj['payment_from'] and vacancies_sj['payment_to']:
-                return int((vacancies_sj['payment_from'] + vacancies_sj['payment_to']) / 2)
-            elif vacancies_sj['payment_from']:
-                return int(vacancies_sj['payment_from'] * 1.2)
-            elif vacancies_sj['payment_to']:
-                return int(vacancies_sj['payment_to'] * 0.8)
-            else:
-                return None
-        else:
-            return None
-
-
-def table(statistic, title):
+def make_table(statistic, title):
     table_data = [
         ["Язык программирования", "Найдено вакансий", "Обработано вакансий", "Средняя зарплата"],
     ]
@@ -45,9 +26,8 @@ def table(statistic, title):
     return table.table
 
 
-def statistic_sj(superjob_token):
-    languages_sj = ["C++", "JavaScript", "Java"]
-    vacancies_sj_json = {}
+def get_statistic_sj(superjob_token, languages_sj):
+    vacancies_statistic_sj = {}
     for language_sj in languages_sj:
         salaries_sj = []
         for page in count(0):
@@ -61,64 +41,70 @@ def statistic_sj(superjob_token):
             }
             response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=headers, params=params)
             response.raise_for_status()
-            vacancies_found_sj = response.json()["total"]
-            if not response.json()["objects"]:
+            vacancies = response.json()
+            vacancies_found_sj = vacancies["total"]
+            if not vacancies["objects"]:
                 break
-            for vacancies_sj in response.json()["objects"]:
-                predicted_salary_sj = predict_rub_salary_sj(vacancies_sj)
+            for vacancies_sj in vacancies["objects"]:
+                predicted_salary_sj = get_predict_rub_salary(vacancies_sj.get('payment_from'), vacancies_sj.get('payment_to'))
                 if predicted_salary_sj:
                     salaries_sj.append(predicted_salary_sj)
         if salaries_sj:
             middle_salary_sj = int(sum(salaries_sj)/len(salaries_sj))
         else:
             middle_salary_sj = None
-        vacancies_sj_json[language_sj] = {
+        vacancies_statistic_sj[language_sj] = {
             "vacancies_found": vacancies_found_sj,
             "vacancies_processed": len(salaries_sj),
             "middle_salary": middle_salary_sj
         }
-    return vacancies_sj_json
+    return vacancies_statistic_sj
 
 
-def statistic_hh():
-    languages_hh = ["Shell", "Swift", "Scala"]
-    vacancies_hh_json = {}
+def get_statistic_hh(languages_hh):
+    vacancies_statistic_hh = {}
+    area = 1
+    period = 30
     for language_hh in languages_hh:
         salaries = []
         for page in count(0):
             params = {
-                "area": 1,
-                "period": 30,
+                "area": area,
+                "period": period,
                 "text": language_hh,
                 "page": page
             }
             response = requests.get('https://api.hh.ru/vacancies', params=params)
             response.raise_for_status()
+            vacancies = response.json()
             if page >= response.json()["pages"]:
                 break
-            vacancies_found_hh = response.json()["found"]
-            for items in response.json()["items"]:
-                salary = items["salary"]
-                predicted_salary_hh = predict_rub_salary_hh(salary)
-                if predicted_salary_hh:
-                    salaries.append(predicted_salary_hh)
+            vacancies_found_hh = vacancies["found"]
+            for salary_inf in vacancies["items"]:
+                salary = salary_inf["salary"]
+                if salary and salary['currency'] == 'RUR':
+                    predicted_salary_hh = get_predict_rub_salary(salary_inf['salary'].get('from'), salary_inf['salary'].get('to'))
+                    if predicted_salary_hh:
+                        salaries.append(predicted_salary_hh)
         if salaries:
             middle_salary_hh = int(sum(salaries) / len(salaries))
         else:
             middle_salary_hh = None
-        vacancies_hh_json[language_hh] = {
+        vacancies_statistic_hh[language_hh] = {
             "vacancies_found": vacancies_found_hh,
             "vacancies_processed": len(salaries),
             "middle_salary": middle_salary_hh
         }
-    return vacancies_hh_json
+    return vacancies_statistic_hh
 
 
 def main():
     load_dotenv()
     superjob_token = os.getenv("SJ_TOKEN")
-    print(table(statistic_sj(superjob_token), "SuperJob"))
-    print(table(statistic_hh(), "HeadHunter"))
+    languages_sj = ["Python", "JavaScript", "Java"]
+    languages_hh = ["Shell", "Swift", "Scala"]
+    print(make_table(get_statistic_sj(superjob_token, languages_sj), "SuperJob"))
+    print(make_table(get_statistic_hh(languages_hh), "HeadHunter"))
 
 
 if __name__ == '__main__':
